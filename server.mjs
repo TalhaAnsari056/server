0882278d7f7972c4c307a2f4a2c7b32687ff9dab
Your Name <TalhaAnsari056>
2025-05-24 23:52:26 +0500

    post-put-delete added

--- server.mjs
+++ server.mjs
@@ -6,6 +6,34 @@ app.get('/', (req, res) => {
     console.log('request IP' + req.ip)
     res.send("Hello ! This is Express server!")
 })
+app.post('/', (req, res) => {
+    console.log("post request !");
+    res.send('post Api');
+})
+
+app.put('/', (req, res) => {
+    console.log("put request !");
+    res.send('put Api');
+})
+
+app.delete('/', (req, res) => {
+    console.log("delete request !");
+    res.send('delete Api');
+})
+
+app.get('/users', (req, res) => {
+    console.log("user route request !");
+    res.send([
+        {
+            name: 'abc',
+            age: 20,
+        },
+        {
+            name: 'xyz',
+            age: 30,
+        },
+    ]);
+})
 
 app.listen(PORT, () => {
     console.log(`my server is running on this port:${PORT}`)
